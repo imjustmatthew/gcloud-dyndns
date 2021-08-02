31dfcdbb727db0bc0f80ca41d48dea358fb6887e
Bohdan Tkachenko <bktkachenko@gmail.com>
2021-08-01 18:38:11 -0700

    Add logging support.

--- gcloud_dynamic_dns/dns.py
+++ gcloud_dynamic_dns/dns.py
@@ -1,6 +1,7 @@
 import socket
 from typing import Set, Iterable, Optional
 
+import logging
 import requests
 import requests.packages.urllib3.util.connection as urllib3_cn
 from google.cloud import dns
@@ -42,7 +43,7 @@ def my_ip(kind: Iterable[socket.AddressFamily] = IPV4_6, query_host: str = "ifco
     return ip_addresses
 
 
-def update_dns(zone_name: str, dns_name: str, ttl: int = 60, force_update: bool = False,
+def update_dns(logger: logging.Logger, zone_name: str, dns_name: str, ttl: int = 60, force_update: bool = False,
                project_id: Optional[str] = None, credentials: Optional[str] = None):
     """
     Updates a GCP Cloud DNS zone with the host's current IP as it appears from the internet
@@ -58,16 +59,23 @@ def update_dns(zone_name: str, dns_name: str, ttl: int = 60, force_update: bool 
     if not dns_name.endswith("."):
         dns_name = "%s." % dns_name
 
-    if len(resolve_addresses(dns_name).symmetric_difference(my_ip())) == 0 and not force_update:
-        return
+    addresses = my_ip()
+    logger.info("Found these external IP addresses: [%s]" % ", ".join(addresses))
+
+    existing_addresses = resolve_addresses(dns_name)
+    logger.info("Found these IP addresses corresponding to domain '%s': [%s]" % (dns_name, ", ".join(existing_addresses)))
+
+    if len(existing_addresses.symmetric_difference(addresses)) == 0 and not force_update:
+        logger.info("Exiting early because IP addresses did not change and force_update is not specified.")
+        return None
 
     if credentials is not None:
+        logger.info("Service account data is provided specifically. Resolving credentials...")
         credentials = service_account.Credentials.from_service_account_info(credentials)
 
     client = dns.Client(project = project_id, credentials = credentials)
     zone = client.zone(zone_name)
 
-    addresses = my_ip()
     ipv4_addresses = set(filter(lambda _ip: len(_ip.split(".")) == 4, addresses))
     ipv6_addresses = addresses - ipv4_addresses
 
@@ -75,11 +83,15 @@ def update_dns(zone_name: str, dns_name: str, ttl: int = 60, force_update: bool 
     for record in zone.list_resource_record_sets():
         if record.name == dns_name and record.record_type in ("A", "AAAA"):
             changes.delete_record_set(record)
+            logger.info("Removed DNS record type %s for domain name '%s'" % (record.record_type, record.name))
 
     if len(ipv4_addresses) > 0:
         changes.add_record_set(zone.resource_record_set(dns_name, "A", ttl, list(ipv4_addresses)))
+        logger.info("Added DNS records type %s for domain name '%s': [%s]" % ("A", dns_name, ", ".join(list(ipv4_addresses))))
+
     if len(ipv6_addresses) > 0:
         changes.add_record_set(zone.resource_record_set(dns_name, "AAAA", ttl, list(ipv6_addresses)))
+        logger.info("Added DNS records type %s for domain name '%s': [%s]" % ("AAAA", dns_name, ", ".join(list(ipv6_addresses))))
 
     changes.create()
     return changes
